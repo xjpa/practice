81c1100eea18fa219bf7923e51d75d821db07f73
xjpa <xjpa@proton.me>
2026-06-10 13:15:25 +0800

    grind

--- leetcode/concatenation-of-array.py
+++ leetcode/concatenation-of-array.py
@@ -1,9 +1,15 @@
-# need to create a new array that contains all elements of original to add to new array we can append
-# then its just a matter of looping
+# need to create a new array that contains all elements of 
+# the original to add to new array we can append.
+# after that, then its just a matter of looping
 
 # time: O(n) 
-# time complexity is not O(n^2) sicne the outer loop runs a fixed constant 2x or 2n
-# nested loops only become O(n^2) when both loops grow with input size
+#
+# time complexity is not O(n^2) sicne 
+# the outer loop runs a fixed constant 2x or 2n.
+#
+# nested loops only become O(n^2) 
+# when both loops grow with input size
+#
 # space: O(n)
 
 class Solution:
@@ -17,3 +23,8 @@ class Solution:
 ## alternatively i could just do the following because of python shit
 
 return nums + nums
+
+# alternatively i could approach it without a new array 
+# but i wouldnt be confident of doing that fast in an interview. 
+# its better to approach it with the approach above as
+# i wouldnt have to worry about indexes
