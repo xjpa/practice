50369df46b49efb170d4aae72c6773d36992b414
xjpa <xjpa@proton.me>
2026-06-24 12:08:16 +0800

    grind

--- kattis/spritt.py
+++ kattis/spritt.py
@@ -1,2 +1,15 @@
 # https://open.kattis.com/problems/spritt
 
+n, m = map(int, input().split())
+sum = 0
+for i in range(n):
+    add = int(input())
+    sum += add
+
+# i often mess up boundary-inclusive conditions
+# such as for this i initially submitted 
+# using "<" instead of "<=" because of habit
+if sum <= m:
+    print("Jebb")
+else:
+    print("Neibb")
