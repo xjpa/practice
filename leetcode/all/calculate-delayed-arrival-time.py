200709cd3c696e5bdaa59af56676d43fad6a11a2
xjpa <xjpa@proton.me>
2026-06-25 10:34:32 +0800

    grind

---
+++ leetcode/all/calculate-delayed-arrival-time.py
@@ -0,0 +1,13 @@
+# https://leetcode.com/problems/calculate-delayed-arrival-time/
+
+class Solution:
+    def findDelayedArrivalTime(self, arrivalTime: int, delayedTime: int) -> int:
+        time = arrivalTime + delayedTime
+        if time >= 24:
+            time = time % 24
+        return time
+            
+
+# perhaps more cleaner is  just a one liner like
+
+return (arrivalTime + delayedTime) % 24
