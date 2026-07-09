9fea263eeb19537bfdd13d6ea018b15ba73e05ff
xjpa <xjpa@proton.me>
2026-07-09 13:57:54 +0800

    grind

---
+++ leetcode/patterns/two-pointer/converge/squares-of-a-sorted-array.py
@@ -0,0 +1,23 @@
+# https://leetcode.com/problems/squares-of-a-sorted-array/description/
+
+"""
+approach
+make an empty list
+loop through the input array
+append the previously empty list with the squared
+return the sorted list
+
+time O(n log n) <-- because sorted()/sorting is nlogn
+space O(n)
+"""
+
+class Solution:
+    def sortedSquares(self, nums: List[int]) -> List[int]:
+        new = []
+        for i in nums:
+            new.append(i**2)
+        return sorted(new)
+    
+"""
+approach 2 is two pointer
+"""
