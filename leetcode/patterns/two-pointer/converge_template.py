35a71d67cfd3a84c01cc69091b6c319a7fc943ab
xjpa <xjpa@proton.me>
2026-07-09 14:23:04 +0800

    add template

---
+++ leetcode/patterns/two-pointer/converge_template.py
@@ -0,0 +1,20 @@
+"""
+left starts at beginnig
+right starts at end
+both move to center
+"""
+
+nums = [1,2,3,4,5]
+
+left = 0
+right = len(nums - 1)
+
+while left <= right:
+    if condition:
+        left += 1
+    else:
+        right -= 1
+
+"""
+this is useful for when the needed information is at the edge of a sorted array/string
+"""
